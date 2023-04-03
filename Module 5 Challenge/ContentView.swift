ccccac90849c71e629011f4bca04d86a9f1e360c
alibekt1012 <alibekt1012@gmail.com>
2023-04-03 16:26:40 +0600

    Initial Commit

---
+++ Module 5 Challenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Module 5 Challenge
+//
+//  Created by Almat Alibekov on 03.04.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Module 5 Challenge/Module_5_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Module_5_ChallengeApp.swift
+//  Module 5 Challenge
+//
+//  Created by Almat Alibekov on 03.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct Module_5_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
